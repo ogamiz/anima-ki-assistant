9fcdb36b013f91f128a2d29705e948e9fe790cb3
ogamiz <105943404+ogamiz@users.noreply.github.com>
2026-08-25 09:20:12 +0200

    Delete anima-ki-assistant directory

--- anima-ki-assistant/service-worker.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'anima-ki-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
-});
